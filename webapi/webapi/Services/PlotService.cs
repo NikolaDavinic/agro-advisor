608ba1376b78f3f7a17a83412ef682c3f2f813cf
Milan Stojkovic <milanstojkovic2707@gmail.com>
2023-02-06 16:09:46 +0100

    Plot Controller POST

---
+++ webapi/webapi/Services/PlotService.cs
@@ -0,0 +1,40 @@
+﻿using BookStoreApi.Services;
+using MongoDB.Driver.GeoJsonObjectModel;
+using webapi.DTO;
+using webapi.Models;
+
+namespace webapi.Services
+{
+    public class PlotService
+    {
+        private readonly IDbContext _context;
+        private readonly IConfiguration _config;
+        public PlotService(
+            IDbContext context,
+            IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        public async Task CreateAsync(PlotDTO newPlot)
+        {
+            var plot = new Plot
+            {
+                Area = newPlot.Area,
+                CurrentCulture = newPlot.CurrentCulture,
+                Harvests = new List<Harvest>(),
+                Municipality = newPlot.Municipality,
+                PlotNumber = newPlot.PlotNumber,
+                UserId = newPlot.UserId,
+                BorderPoints = new List<GeoJsonPoint<GeoJson2DGeographicCoordinates>>()
+            };
+
+            newPlot.BorderPoints.ForEach(point =>
+            {
+                var loc = GeoJson.Point(new GeoJson2DGeographicCoordinates(point.X, point.Y));
+                plot.BorderPoints.Add(loc);
+            });
+            await _context.Plots.InsertOneAsync(plot);
+        }
+    }
+}
