5915fbf1e6cbc9368440e9ee2b1cbb09a7bd6860
stefan-foo <stojadinovicstefan59@gmail.com>
2023-02-11 15:28:41 +0100

    izmena berba kartice

--- client/src/components/HarvestForm/HarvestForm.tsx
+++ client/src/components/HarvestForm/HarvestForm.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Stack, TextField } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import moment from "moment";
-import { Controller, useForm } from "react-hook-form";
-import { Harvest } from "../../models/harvest.model";
-import MatIcon from "../MatIcon/MatIcon";
+import { Box, Button, Stack, TextField } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import moment from 'moment';
+import { Controller, useForm } from 'react-hook-form';
+import { Harvest } from '../../models/harvest.model';
+import MatIcon from '../MatIcon/MatIcon';
 
 interface FormFields {
   cultureName?: string;
@@ -17,9 +17,7 @@ interface HarvestFormProps {
   onSubmit: (harvest: Harvest) => void;
 }
 
-const HarvestForm = ({
-  onSubmit = () => { },
-}: HarvestFormProps) => {
+const HarvestForm = ({ onSubmit = () => {} }: HarvestFormProps) => {
   const {
     register,
     handleSubmit,
@@ -29,62 +27,47 @@ const HarvestForm = ({
   } = useForm<FormFields>({
     defaultValues: {
       amount: 0,
-      cultureName: "",
-      date: new Date()
+      cultureName: '',
+      date: new Date(),
     },
-    reValidateMode: "onSubmit",
+    reValidateMode: 'onSubmit',
   });
 
   const formSubmit = (data: FormFields) => {
-    onSubmit(
-      {
-        amount: data.amount,
-        cultureName: data.cultureName,
-        date: data.date
-      }
-    );
+    onSubmit({
+      amount: data.amount,
+      cultureName: data.cultureName,
+      date: data.date,
+    });
   };
 
   return (
-    <Stack
-      component="form"
-      onSubmit={handleSubmit(formSubmit)}
-      className="gap-2"
-    >
+    <Stack component='form' onSubmit={handleSubmit(formSubmit)} className='gap-2'>
       <Controller
         control={control}
-        name="date"
+        name='date'
         render={({ field: { onChange, value } }) => (
           <LocalizationProvider dateAdapter={AdapterMoment}>
             <DatePicker
-              label="Datum berbe"
-              onChange={(val) => onChange(val?.toDate())}
+              label='Datum berbe'
+              onChange={val => onChange(val?.toDate())}
               value={moment(value)}
-              renderInput={(props) => (
-                <TextField size="small" {...props}></TextField>
-              )}
-            ></DatePicker>
+              renderInput={props => <TextField size='small' {...props}></TextField>}></DatePicker>
           </LocalizationProvider>
         )}
       />
+      <TextField size='small' label='Ubrana kultura' {...register('cultureName')}></TextField>
       <TextField
-        size="small"
-        label="Ubrana kultura"
-        {...register("cultureName")}
-      ></TextField>
-      <TextField
-        size="small"
-        label="Količina u kg"
-        type="string"
-        {...register("amount")}
-      ></TextField>
-      <Box className="flex justify-end">
+        size='small'
+        label='Količina u kg'
+        type='string'
+        {...register('amount')}></TextField>
+      <Box className='flex justify-end'>
         <Button
-          color="primary"
-          type="submit"
-          variant="outlined"
-          startIcon={<MatIcon>check</MatIcon>}
-        >
+          color='primary'
+          type='submit'
+          variant='outlined'
+          startIcon={<MatIcon>check</MatIcon>}>
           Dodaj berbu
         </Button>
       </Box>
